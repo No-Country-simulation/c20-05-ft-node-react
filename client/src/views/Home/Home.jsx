ee228fe8ec703f092585c36d16c10ad70fdfda75
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-08-21 11:21:44 -0300

    Mi hola mundo

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-blue-700 text-7xl bg-gray-600 text-center'>QUEDARON CHETAS LAS RUTAS</div>
+    <div className='text-blue-700 text-7xl bg-gray-600 text-center'>QUEDARON CHETAS LAS RUTAS
+    <span>Hola mundo</span>
+    </div>
   )
 }
 
