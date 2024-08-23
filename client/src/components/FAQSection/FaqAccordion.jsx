ea2d532d5f78d57bc5731c6d98132b01733f7e2d
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-08-23 18:08:26 -0300

    feature: Se mejoró las faq con transiciones todo con css, sin js ni react

--- client/src/components/FAQSection/FaqAccordion.jsx
+++ client/src/components/FAQSection/FaqAccordion.jsx
@@ -4,10 +4,18 @@ const FaqAccordion = () => {
   return (
     <div className="flex-1">
       {FAQ.map((faq, index) => (
-        <details key={index} className="bg-gray-400 flex gap-4 p-4">
-          <summary className="text-xl text-white">{faq.icon} {faq.question}</summary>
-          <p className="text-sm text-white p-2">{faq.answer}</p>
-        </details>
+        <div key={index} className="bg-gray-400 flex gap-4 p-4">
+          <div className="h-max overflow-hidden">
+            <input id={`accordion-checkbox-${index}`} type="checkbox" className="hidden ml-4 [&+label+div>p]:checked:translate-y-0 [&+label+div>p]:checked:h-fit [&+label>img]:checked:rotate-180" />
+            <label htmlFor={`accordion-checkbox-${index}`} className="relative bg-gray-500 flex gap-2 items-center z-10">
+              <img src="src/assets/svg/chevron-icon.svg" alt="ver info" className="size-6 invert transition-transform ease-out duration-300" /> 
+              <h4 className="text-xl text-white">{faq.icon} {faq.question}</h4>
+            </label>
+            <div>
+              <p className="h-0 -translate-y-28 text-sm text-white p-2 transition-transform ease-out duration-300">{faq.answer}</p>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   )
