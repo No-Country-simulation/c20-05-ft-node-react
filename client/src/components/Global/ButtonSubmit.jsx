488a07d5eaaf90e7606d6c5ad2fc4b421ccc03ac
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-08-29 01:19:37 -0300

    Guardando

--- client/src/components/Global/ButtonSubmit.jsx
+++ client/src/components/Global/ButtonSubmit.jsx
@@ -1,5 +1,5 @@
 const ButtonSubmit = ({ children, className, extraClassName = '', disabled }) => {
-  const classDefault = `bg-btn text-white font-medium rounded-lg py-2 mt-4 border-2 border-btn transition-colors ease-out duration-300 hover:bg-white hover:text-btn disabled:opacity-30 ${extraClassName}` 
+  const classDefault = `bg-btn text-white font-medium rounded-lg py-2 border-2 border-btn transition-colors ease-out duration-300 hover:bg-white hover:text-btn disabled:opacity-30 ${extraClassName}` 
   const classes = className ? `${className}` : classDefault
   return <button type='submit' className={classes} disabled={disabled}>{children}</button>
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "primary": "#4a90e2",
         "gray-light": "#D9D9D9",
         "gray-darkCalendar": "#353535",
-        "btn": "#57886C",
+        "btn": "#4a90e2",
+        "principal-text": "#333333",
+        "secondary-text": "#9b9b9b",
+        "text-error": "#e94e77",
+        "page-background": "#f5f5f5",
       },
       boxShadow: {
         container: 'rgba(0, 0, 0, 0.15) 0px 0px 3px'
