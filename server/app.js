1c22f9f17e40b8c290ed8c3f92810100794e9768
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-08-29 14:22:47 -0300

    fix: Se agregó server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,15 @@ import express from "express";
 import MongoSingleton from "./src/config/mongoDB.js";
 import { entorno } from "./src/config/config.js";
 import { Server } from "socket.io";
-
+import cors from "cors";
+import routerApp from "./src/routes/userRouter.js";
 const PORT = entorno.port;
 const app = express();
+// cors
+app.use(cors({ origin: "*" }));
+
+app.use(express.json())
+app.use(routerApp)
 
 const server = app.listen(PORT, () => {
 	console.log(`Servidor en ejecución en http://localhost:${PORT}`);
