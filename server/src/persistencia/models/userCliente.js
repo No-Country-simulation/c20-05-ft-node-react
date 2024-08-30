a2d91925421468774a14a89d03a5151f5c25a7be
thomilopez <thomasignaciolopez@gmail.com>
2024-08-29 23:24:46 -0300

    cambio en el modelo de usuario

--- server/src/persistencia/models/userCliente.js
+++ server/src/persistencia/models/userCliente.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose'
 
 const UserSchema = new Schema({
 	first_name: { type: String, trim: true, required: true },
@@ -10,8 +10,10 @@ const UserSchema = new Schema({
 	city: { type: String, required: false },
 	zipCode: { type: Number, required: false },
 	isadmin: { type: Boolean, default: false },
-	role: { type: String, enum: ["owner", "carer"], default: "owner" },
+	role: { type: String, enum: ['owner', 'carer'], default: 'owner' },
+	profilePicture: { type: String, required: false }, // URL de la foto de perfil
+	images: { type: [String], required: false }, // URLs de otras fotos
 	// pet: [{ type: Schema.Types.ObjectId, ref: 'pets' }],
-});
+})
 
-export const UserModel = model("user", UserSchema);
+export const UserModel = model('user', UserSchema)
