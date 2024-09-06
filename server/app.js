98e2ba642c55bc142dd472c52d7df84a67209e1b
thomilopez <thomasignaciolopez@gmail.com>
2024-09-06 00:33:42 -0300

    agregado de swagger para las rutas de cuidador

--- server/app.js
+++ server/app.js
@@ -6,11 +6,29 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import routerApp from "./src/routes/index.js";
 import { errorHandler, notFound } from "./src/middlewares/errorHandler.js";
-
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
+import __dirname from "../server/src/utils/utils.js";
 // import { Server } from "socket.io";
 
 const PORT = entorno.port ?? 8080;
 const app = express();
+
+// Configuracion de swagger
+const swaggerOptions = {
+	definition: {
+		openapi: "3.0.1",
+		info: {
+			title: "API de cuidadores de mascotas ",
+			description: "API de cuidadores de mascotas",
+		},
+	},
+	apis: [`${__dirname}/../docs/**/*.yaml`],
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
+
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.use(cors());
 app.use(cookieParser());
@@ -27,20 +45,3 @@ const server = app.listen(PORT, () => {
 });
 
 MongoSingleton.getInstance();
-
-// Configuracion de swagger
-
-const swaggerOptions = {
-	definition: {
-		openapi: "3.0.1",
-		info: {
-			title: "API de cuidadores de mascotas ",
-			description: "API de cuidadores de mascotas",
-		},
-	},
-	apis: [`${__dirname}/docs/**/*.yaml`],
-};
-
-const specs = swaggerJSDoc(swaggerOptions);
-
-app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
