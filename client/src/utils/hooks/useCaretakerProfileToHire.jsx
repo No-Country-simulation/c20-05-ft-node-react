25e9d664cfed2a70f6931d71ebe3bb40d2b6b690
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-09-09 18:14:30 -0300

    feature: Se agregó el select para editar el tipo de servicio en el resumen de contratación

--- client/src/utils/hooks/useCaretakerProfileToHire.jsx
+++ client/src/utils/hooks/useCaretakerProfileToHire.jsx
@@ -3,8 +3,11 @@ import { serviceCareTakerValidations } from "../functions/validations/serviceCar
 import { SERVICE_NAME_AND_DATE_VALUES } from "../../assets/other-assets/constants/form-resources"
 import useNavigateHelper from "./useNavigateHelper"
 import { useDispatch } from "react-redux"
+import { getServicesForOptions } from "../functions/getServicesForOptions"
+import { caretakerProfileMock } from "../../assets/other-assets/mocks/mockForProfile"
 
 export const useCaretakerProfileToHire = () => {
+  const { serviceType, coordinates } = caretakerProfileMock
   const [servicesOptions, setServicesOptions] = useState([])
   const [service, setService] = useState(SERVICE_NAME_AND_DATE_VALUES)
   const [errors, setErrors] = useState()
@@ -13,8 +16,12 @@ export const useCaretakerProfileToHire = () => {
   const goToNextStep = () => navigateTo('./hire')
 
   useEffect(() => {
+    setServicesOptions(getServicesForOptions(serviceType))
+  }, [])
+
+  useEffect(() => {
     serviceCareTakerValidations(service, setErrors)
   }, [service])
 
-  return { servicesOptions, setServicesOptions, service, setService, errors, setErrors, dispatch, goToNextStep }
+  return { servicesOptions, setServicesOptions, service, setService, errors, setErrors, dispatch, goToNextStep, coordinates }
 }
